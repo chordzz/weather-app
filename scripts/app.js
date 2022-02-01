e5dc5f1e582210b7dd5daf57a2819a6ffef821b4
Olaife Olawore <olaifeolawore@gmail.com>
2022-02-01 13:48:20 +0100

    added local storage to app

--- scripts/app.js
+++ scripts/app.js
@@ -60,4 +60,14 @@ cityForm.addEventListener("submit", e => {
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 
+  
+  //Set local storage
+  localStorage.setItem('city', city);
+
 });
+
+if(localStorage.getItem('city')){
+  updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
